e2153c03a1d66bb49441325760b69c0b05df5612
Tatieonna <tatieonnadestiny1999@icloud.com>
2023-10-23 14:16:38 -0500

    new methods, home screen, csv file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Home screen: ");
+        System.out.println("D): Make a deposit");
+        System.out.println("P): Make a payment");
+        System.out.println("L): See ledger");
+        System.out.println("X): Exit application");
+
+
+
+        String userInput = scanner.nextLine();
+        switch (userInput){
+            case "D":
+                System.out.println("Enter deposit information");
+                break;
+            case "P":
+                System.out.println("Enter debit information");
+                break;
+            case "L":
+                //display ledger screen
+                break;
+            case "X":
+                System.out.println("Thank you!");
+
+
+        }
+
+        }
+       public static String deposit (){
+        return
+       }
+       public static String payment(){
+        return
+       }
+        public static String ledger(){
+        return
+        }
+        public static String exit(){
+        return
+        }
+
 }
